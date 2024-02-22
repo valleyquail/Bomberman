4b3e8dd87ddd0b08b99f3f5d424dac9b2a1761e0
JaredMorgan21 <jmmorgan1007@gmail.com>
2024-02-22 13:56:00 -0500

    created q-learning format

--- team03/testcharacter.py
+++ team03/testcharacter.py
@@ -1,6 +1,8 @@
 # This is necessary to find the main code
 import math
+import random
 import sys
+import numpy as np
 sys.path.insert(0, '../bomberman')
 # Import necessary stuff
 from entity import CharacterEntity
@@ -8,101 +10,36 @@ from colorama import Fore, Back
 from priority_queue import PriorityQueue
 
 class TestCharacter(CharacterEntity):
+    action = "down"
     def do(self, wrld):
-        # Your code here
-        path = self.a_star(wrld)
-        print("path:", path)
+        Qs = np.load("qs.npy", allow_pickle=True).item()
+        self.next_move(wrld, Qs)
+        action = self.action
+        print(Qs, action)
+        if action == "up left":
+            self.move(-1, -1)
+        elif action == "up":
+            self.move(0, -1)
+        elif action == "up right":
+            self.move(1, -1)
+        elif action == "left":
+            self.move(-1, 0)
+        elif action == "stay":
+            self.move(0, 0)
+        elif action == "right":
+            self.move(1, 0)
+        elif action == "down left":
+            self.move(-1, 1)
+        elif action == "down":
+            self.move(0, 1)
+        elif action == "down right":
+            self.move(-1, 1)
+        elif action == "bomb":
+            self.place_bomb()
 
-        self.move(path[0][0] - self.x, path[0][1] - self.y)
 
-        pass
 
-    def a_star(self, wrld):
-        goal = self.goal(wrld)
-        start = (self.x, self.y)
-
-        monster_paths = []
-        for monster in self.monsters(wrld):
-            monster_paths.append(self.monster_a_star(monster, start, wrld))
-
-        locs = PriorityQueue()
-        locs.put(start, 0)
-
-        came_from = {}
-        monsters = {}
-        cost_so_far = {}
-        came_from[start] = None
-        cost_so_far[start] = 0
-        monsters[start] = [monster for monster in self.monsters(wrld)]
-
-        path = []
-
-        while not locs.empty():
-            loc = locs.get()
-
-            x = loc[0]
-            y = loc[1]
-
-            if wrld.exit_at(x, y):
-                path.insert(0, loc)
-                node = loc
-
-                while came_from[node] is not None:
-                    path.insert(0, node)
-                    node = came_from[node]
-
-                return path
-
-            if wrld.empty_at(x, y) or (x, y) == (self.x, self.y):
-                cost = cost_so_far[loc]
-                adj_cells = [(x + 1, y), (x + 1, y + 1), (x + 1, y - 1), (x - 1, y), (x - 1, y + 1), (x - 1, y - 1), (x, y + 1), (x, y - 1)]
-
-                for next in adj_cells:
-                    if -1 < next[0] < wrld.width() and -1 < next[1] < wrld.height() and (wrld.empty_at(next[0], next[1]) or wrld.exit_at(next[0], next[1])) and next not in came_from.values():
-
-                        if monster_paths:
-                            monster_paths = []
-                            for monster in monsters[loc]:
-                                monster_paths.append(self.monster_a_star(monster, next, wrld))
-
-                            nearest_monster = 0
-                            for monster in monster_paths:
-                                dist = self.distance(next, monster[1])
-                                weight = 1000000 * 100 ** (-dist)
-
-                                if weight > nearest_monster:
-                                    nearest_monster = weight
-
-                            new_cost = cost + nearest_monster + 1
-                            if next in cost_so_far:
-                                if cost_so_far[next] > cost + self.distance(next, goal):
-
-                                    locs.put(next, new_cost + self.distance(next, goal))
-                                    came_from[next] = loc
-                                    cost_so_far[next] = new_cost
-                                    monsters[next] = [path[0] for path in monster_paths]
-                            else:
-                                locs.put(next, cost + nearest_monster + self.distance(next, goal))
-                                came_from[next] = loc
-                                cost_so_far[next] = cost + nearest_monster + 1
-                                monsters[next] = [path[0] for path in monster_paths]
-
-                        else:
-                            if next in cost_so_far:
-                                if cost_so_far[next] > cost + self.distance(next, goal):
-                                    locs.put(next, cost + self.distance(next, goal))
-                                    came_from[next] = loc
-                                    cost_so_far[next] = cost + self.distance(next, loc)
-                            else:
-                                locs.put(next, cost + self.distance(next, goal))
-                                came_from[next] = loc
-                                cost_so_far[next] = cost + self.distance(next, loc)
-
-        return path
-
-    def monster_a_star(self, monster, player, wrld):
-        goal = player
-        start = monster
+    def a_star(self, goal, start, wrld):
 
         locs = PriorityQueue()
         locs.put(start, 0)
@@ -114,9 +51,11 @@ class TestCharacter(CharacterEntity):
 
         path = []
 
+        if not goal or not start:
+            return
+
         while not locs.empty():
             loc = locs.get()
-            # print(loc)
 
             x = loc[0]
             y = loc[1]
@@ -128,22 +67,26 @@ class TestCharacter(CharacterEntity):
                 while came_from[node] is not None:
                     path.insert(0, node)
                     node = came_from[node]
-                path.insert(0, node)
 
                 return path
 
-            if wrld.empty_at(x, y) or (x, y) == (monster[0], monster[1]):
+            if wrld.empty_at(x, y) or (x, y) == (start[0], start[1]):
                 cost = cost_so_far[loc]
                 adj_cells = [(x + 1, y), (x + 1, y + 1), (x + 1, y - 1), (x - 1, y), (x - 1, y + 1), (x - 1, y - 1), (x, y + 1), (x, y - 1)]
 
                 for next in adj_cells:
+                    if -1 < next[0] < wrld.width() and -1 < next[1] < wrld.height() and (wrld.empty_at(next[0], next[1]) or next == goal) and next not in came_from.values():
+                        if next in cost_so_far:
+                            if cost_so_far[next] > cost + self.distance(next, goal):
+                                locs.put(next, cost + self.distance(next, goal))
+                                came_from[next] = loc
+                                cost_so_far[next] = cost + 1
+                        else:
+                            locs.put(next, cost + self.distance(next, goal))
+                            came_from[next] = loc
+                            cost_so_far[next] = cost + 1
 
-                    if -1 < next[0] < wrld.width() and -1 < next[1] < wrld.height() and (wrld.empty_at(next[0], next[1]) or next == goal) and next not in came_from:
-                        locs.put(next, cost + self.distance(next, goal))
-                        came_from[next] = loc
-                        cost_so_far[next] = cost + self.distance(loc, next)
-
-        return path
+        return
 
     def distance(self, p1, p2):
         return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
@@ -154,6 +97,23 @@ class TestCharacter(CharacterEntity):
                 if wrld.exit_at(x, y):
                     return (x, y)
 
+    def bomb(self, wrld):
+        for x in range(wrld.width()):
+            for y in range(wrld.height()):
+                if wrld.bomb_at(x, y):
+                    return (x, y)
+
+    def explosions(self, wrld):
+        explosions = []
+
+        for x in range(wrld.width()):
+            for y in range(wrld.height()):
+                if wrld.explosion_at(x, y):
+                    explosions.append((x, y))
+
+        return explosions
+
+
     def monsters(self, wrld):
         monsters = []
         for x in range(wrld.width()):
@@ -162,3 +122,101 @@ class TestCharacter(CharacterEntity):
                     monsters.append((x, y))
 
         return monsters
+
+    def calc_values(self, wrld):
+        goal = self.goal(wrld)
+        path_to_goal = self.a_star(goal, (self.x, self.y), wrld)
+
+        if path_to_goal:
+            goal_dist = len(path_to_goal)
+            goal_ang = math.atan2(self.y - goal[1], self.x - goal[0])
+        else:
+            goal_dist = 26
+            goal_ang = 0
+
+
+        monster_paths = []
+        for monster in self.monsters(wrld):
+            result = self.a_star((self.x, self.y), monster, wrld)
+            if result is not None:
+                monster_paths.append(result)
+
+        if monster_paths:
+            monster_dist = len(monster_paths[0])
+            monster_ang = math.atan2(monster_paths[0][0][1] - self.y, monster_paths[0][0][0] - self.x)
+            for path in monster_paths:
+                if len(path) < monster_dist:
+                    monster_dist = len(path)
+                    monster_ang = math.atan2(path[0][1] - self.y, path[0][0] - self.x)
+        else:
+            monster_dist = 26
+            monster_ang = 0
+
+
+        explosion_paths = []
+        for explosion in self.explosions(wrld):
+            result = self.a_star((self.x, self.y), explosion, wrld)
+            if result is not None:
+                explosion_paths.append(result)
+
+        if explosion_paths:
+            explosion_dist = len(explosion_paths[0])
+            explosion_ang = math.atan2(explosion_paths[0][0][1] - self.y, explosion_paths[0][0][0] - self.x)
+            for path in explosion_paths:
+                if len(path) < explosion_dist:
+                    explosion_dist = len(path)
+                    explosion_ang = math.atan2(path[0][1] - self.y, path[0][0] - self.x)
+        else:
+            explosion_dist = 26
+            explosion_ang = 0
+
+
+        bomb = self.bomb(wrld)
+        path_to_bomb = self.a_star(bomb, (self.x, self.y), wrld)
+
+        if path_to_bomb:
+            bomb_dist = len(path_to_bomb)
+            bomb_ang = math.atan2(self.y - bomb[1], self.x - bomb[0])
+        else:
+            bomb_dist = 26
+            bomb_ang = 45
+
+        bomb_danger = 0
+        if bomb_ang == 0 or abs(bomb_ang - math.pi/2) < 0.01 or abs(bomb_ang - math.pi) < 0.01 or abs(bomb_ang - 3*math.pi/2) < 0.01:
+            bomb_danger = 1
+
+        return goal_dist, monster_dist, explosion_dist, bomb_dist, bomb_danger
+
+    def next_move(self, wrld, Qs):
+        state = self.calc_values(wrld)
+        sum = 0
+        min = 10000
+
+        actions = ["up left", "up", "up right",
+                   "left", "stay", "right",
+                   "down left", "down", "down right", "bomb"]
+        action_dict = {}
+        for action in actions:
+            if (state, action) in Qs:
+                sum += max(Qs[state, action], 0)
+                action_dict[action] = max(Qs[state, action], 0)
+
+            else:
+                sum += 1
+                action_dict[action] = 1
+
+        count = 0
+        if sum != 10:
+            rand = random.random() * sum
+            for action in action_dict:
+                if count + action_dict[action] > rand:
+                    self.action = action
+                    return
+                count += action_dict[action]
+        else:
+            rand = random.randint(1, 10)
+            self.action = actions[rand-1]
+            return
+
+    def get_next_move(self):
+        return self.action
---
+++ team03/training.py
@@ -0,0 +1,113 @@
+# This is necessary to find the main code
+import math
+import sys
+
+import numpy as np
+import pygame
+import colorama
+np.save('qs.npy', {})
+
+with open('weights.txt', 'r') as fd:
+    w_goal_dist = int(fd.readline().split()[2])
+    w_goal_ang = int(fd.readline().split()[2])
+    w_monster_dist = int(fd.readline().split()[2])
+    w_monster_ang = int(fd.readline().split()[2])
+    w_explosion_dist = int(fd.readline().split()[2])
+    w_explosion_ang = int(fd.readline().split()[2])
+    w_bomb_dist = int(fd.readline().split()[2])
+    w_bomb_ang = int(fd.readline().split()[2])
+    w_bomb_danger = int(fd.readline().split()[2])
+
+weights = [w_goal_dist, w_monster_dist, w_explosion_dist, w_bomb_dist, w_bomb_danger]
+
+sys.path.insert(0, '../bomberman')
+sys.path.insert(1, '..')
+
+# Import necessary stuff
+from events import Event
+from game import Game
+from monsters.stupid_monster import StupidMonster
+
+# TODO This is your code!
+sys.path.insert(1, '../team03')
+from testcharacter import TestCharacter
+
+# Run!
+n_steps = 10000
+gamma = 0.8
+Q = {}
+prevState = None
+prevAction = None
+alpha = 0.0000001
+
+for step in range(n_steps):
+    print(step)
+    # Create the game
+    g = Game.fromfile('project2/map.txt', sprite_dir="../bomberman/sprites/")
+
+    character = TestCharacter("me" + str(step),  # name
+                              "C",  # avatar
+                              0, 0  # position
+                              )
+    # TODO Add your character
+    g.add_character(character)
+    g.add_monster(StupidMonster("stupid",  # name
+                                "S",  # avatar
+                                3, 9  # position
+                                ))
+
+
+    """ Main game loop. """
+    def step():
+        # pygame.event.clear()
+        # input("Press Enter to continue or CTRL-C to stop...")
+        pygame.time.wait(abs(1))
+
+    colorama.init(autoreset=True)
+    g.display_gui()
+    g.draw()
+    step()
+
+    while not g.done():
+        prevState = character.calc_values(g.world)
+        prevAction = character.action
+
+        (g.world, g.events) = g.world.next()
+        g.display_gui()
+        g.draw()
+        step()
+        g.world.next_decisions()
+
+        load = np.load("qs.npy", allow_pickle=True).item()
+        state = character.calc_values(g.world)
+        next_move = character.action
+
+        Q[state, next_move] = 0
+        for i, val in enumerate(state):
+            Q[state, next_move] += weights[i] * val
+        print(g.world.events)
+        if (prevState, prevAction) in Q:
+            r = -1
+            for e in g.world.events:
+                if e.tpe == Event.BOMB_HIT_CHARACTER:
+                    r -= 100
+                elif e.tpe == Event.BOMB_HIT_WALL:
+                    r += 100
+                elif e.tpe == Event.BOMB_HIT_MONSTER:
+                    r += 100
+                elif e.tpe == Event.CHARACTER_KILLED_BY_MONSTER:
+                    r -= 100
+                elif e.tpe == Event.CHARACTER_FOUND_EXIT:
+                    r += 100000
+
+            delta = r + gamma * Q[state, next_move] - Q[prevState, prevAction]
+            print(r)
+            for i, weight in enumerate(weights):
+                weights[i] += alpha * delta * state[i]
+
+    np.save('qs.npy', Q)
+
+
+    colorama.deinit()
+    # g.go(1)
+np.save('qs.npy', Q)
